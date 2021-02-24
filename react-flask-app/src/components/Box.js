3de68a783eb5de8784f8fe67509b5569a64ff4f7
Marc <mecquilatan@gmail.com>
2021-02-23 22:41:55 -0500

    added an add and remove function

--- react-flask-app/src/components/Box.js
+++ react-flask-app/src/components/Box.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Spring } from "react-spring/renderprops";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const box = 
-<svg width="125" height="125">
+const box = (
+  <svg width="125" height="125">
     <defs>
-        <linearGradient id="MyGradient">
-            <stop  stopColor="pink" /><stop offset="99.9%" stopColor='#ECE6EC' />
-        </linearGradient>
+      <linearGradient id="MyGradient">
+        <stop stopColor="pink" />
+        <stop offset="99.9%" stopColor="#ECE6EC" />
+      </linearGradient>
     </defs>
-    <rect fill="url(#MyGradient)" x="10" y="10" width="100" height="100" rx="15"  />
-</svg>
+    <rect
+      fill="url(#MyGradient)"
+      x="10"
+      y="10"
+      width="100"
+      height="100"
+      rx="15"
+    />
+  </svg>
+);
 
-export default box
+const animatedBox = (index) => {
+  return (
+    <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+      {(props) => (
+        <>
+          <Col className="test d-flex justify-content-center" style={props}>
+            <div>
+              <Row>{box}</Row>
+              <Row className="test d-flex justify-content-center">
+                <h1>{index + 1}</h1>
+              </Row>
+            </div>
+          </Col>
+        </>
+      )}
+    </Spring>
+  );
+};
+
+const boxElements = (num) => {
+  let boxList = [];
+  for (let i = 0; i < num; i++) {
+    boxList.push(animatedBox(i));
+  }
+  return boxList;
+};
+
+export default boxElements;
--- react-flask-app/src/index.js
+++ react-flask-app/src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { ReactSVG } from 'react-svg'
-import {Spring} from 'react-spring/renderprops'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import box from './components/Box'
+import "bootstrap/dist/css/bootstrap.min.css";
+import boxElements from "./components/Box";
+import Button from "react-bootstrap/Button";
+import { getAppendedData, getInitialData, getTruncatedData } from "./helpers";
 
 // Import bootstrap components
 import { Container, Row, Col } from "react-bootstrap";
@@ -12,42 +12,45 @@ const rootElement = document.getElementById("root");
 
 // Variables
 
+const App = () => {
+  const [numBox, setNumBox] = useState(1);
 
-const animatedBox = <Spring
-  from={{ opacity: 0 }}
-  to={{ opacity: 1 }}>
-  {props => 
-  <>
-  
-  <Col className="test d-flex justify-content-center" style={props}>
-      <div>
-          <Row>{box}</Row>
-        <Row className="test d-flex justify-content-center"><h1>4</h1></Row>
-      </div>
-      </Col>
-  
-  </>}
-</Spring>
-
-function App () {
-    return (
-        <Container fluid>
-            <Row>
-                <Col className="d-flex justify-content-center">
-                    <h1>Hello World!</h1>
-                </Col>
-            </Row>
-            <Row className="justify-content-center">        
-                    {animatedBox}     
-                    {animatedBox} 
-                    {animatedBox}         
-            </Row>
-          
-        </Container>
-    
-    ) 
-    
-}
-
+  console.log(numBox);
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col className="d-flex justify-content-center">
+            <h1>Hello World!</h1>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">{boxElements(numBox)}</Row>
+        <Row className="justify-content-center ">
+          <Button
+            className="btn mr-2"
+            variant="primary"
+            onClick={() => setNumBox(numBox + 1)}
+          >
+            Add
+          </Button>
+          <Button
+            className="btn mr-2"
+            variant="primary"
+            onClick={() => setNumBox(numBox - 1)}
+          >
+            Remove
+          </Button>
+          <Button
+            className="btn"
+            variant="primary"
+            onClick={() => setNumBox(1)}
+          >
+            Reset
+          </Button>
+        </Row>
+      </Container>
+    </>
+  );
+};
 
 ReactDOM.render(<App />, rootElement);
